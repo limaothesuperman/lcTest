eae0f542462cc2729a16c7f2e6ceeb78090da5d4
Mao Li <mli94@stevens.edu>
2022-05-09 10:44:58 -0400

    add RansomNote_383

---
+++ src/RansomNote_383.java
@@ -0,0 +1,14 @@
+public class RansomNote_383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] record = new int[26];
+        for (char c : magazine.toCharArray())
+            record[c - 'a']++;
+        for (char c : ransomNote.toCharArray()) {
+            record[c - 'a']--;
+            if (record[c - 'a'] < 0)
+                return false;
+        }
+        return true;
+
+    }
+}
