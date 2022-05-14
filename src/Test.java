8773bbf31cc87ad60146e687d2068de0a910af2c
Mao Li <mli94@stevens.edu>
2022-05-14 18:46:56 -0400

    add ReverseString_344

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,25 @@
 public class Test {
     public static void main(String[] args) {
-        FourSum4_18.testCase();
+//        int i = 0;
+//        int j = 1;
+//        int[] s = new int[2];
+//        s[0] = 1;
+//        s[1] = 2;
+//
+//        for (int k = 0; k < s.length; k++) {
+//            System.out.println(s[k]);
+//
+//        }
+//        s[i] = s[j] ^ s[i];
+//        s[j] = s[i] ^ s[j];
+//        s[i] = s[j] ^ s[i];
+//
+//        for (int k = 0; k < s.length; k++) {
+//            System.out.println(s[k]);
+//
+//        }
+        int s = 5;
+        System.out.println(11 ^ 22);
     }
 
 
