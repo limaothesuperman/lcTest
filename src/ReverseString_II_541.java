2c4084211dffeab73b9e1e3474f5c35740e95ccd
Mao Li <mli94@stevens.edu>
2022-05-14 19:18:39 -0400

    add ReverseString_II_541

---
+++ src/ReverseString_II_541.java
@@ -0,0 +1,22 @@
+public class ReverseString_II_541 {
+    public String reverseStr(String s, int k) {
+
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < ch.length; i += 2 * k) {
+            int start = i;
+            int end = Math.min(ch.length - 1, start + k - 1);
+            while (end > start) {
+                ch[start] ^= ch[end];
+                ch[end] ^= ch[start];
+                ch[start] ^= ch[end];
+                start++;
+                end--;
+
+            }
+
+        }
+
+        return new String(ch);
+
+    }
+}
