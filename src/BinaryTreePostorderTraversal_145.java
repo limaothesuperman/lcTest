38904ec681def92e629f7c4dd80a7d7318a91ea2
Mao Li <mli94@stevens.edu>
2023-05-16 16:17:29 -0500

    145. Binary Tree Postorder Traversal

---
+++ src/BinaryTreePostorderTraversal_145.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreePostorderTraversal_145 {
+    private List<Integer> res = new LinkedList<>();
+
+    private void traverse(TreeNode node) {
+        if (node == null) return;
+        if (node.left != null) traverse(node.left);
+        if (node.right != null) traverse(node.right);
+        res.add(node.val);
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        traverse(root);
+        return res;
+
+    }
+}
