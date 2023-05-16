1776d807833b68efd62751ddfeb04e4de1865b4f
Mao Li <mli94@stevens.edu>
2023-05-16 16:11:45 -0500

    added 144. Binary Tree Preorder Traversal

---
+++ src/BinaryTreePreorderTraversal_144.java
@@ -0,0 +1,53 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BinaryTreePreorderTraversal_144 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    private List<Integer> res = new LinkedList<>();
+
+    private void dfs(TreeNode node) {
+        if (node == null) return;
+        res.add(node.val);
+        dfs(node.left);
+        dfs(node.right);
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+}
+
