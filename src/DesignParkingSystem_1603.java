605acbb0d5a8d8b5f769ef574d31c1093bfbce3b
Mao Li <mli94@stevens.edu>
2023-05-29 17:56:51 -0500

    added 1603. Design Parking System

---
+++ src/DesignParkingSystem_1603.java
@@ -0,0 +1,41 @@
+public class DesignParkingSystem_1603 {
+
+
+}
+
+class ParkingSystem {
+
+    public int big;
+    public int medium;
+    public int small;
+
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+
+    }
+
+    public boolean addCar(int carType) {
+        if (carType == 1 && this.big >= 1) {
+            this.big--;
+            return true;
+        } else if (carType == 2 && this.medium >= 1) {
+            this.medium--;
+            return true;
+        } else if (carType == 3 && this.small >= 1) {
+            this.small--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
+
